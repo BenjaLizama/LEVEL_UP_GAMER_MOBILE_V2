68c05007fb089f24120e0f8d1994be9d0f7cce1d
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-10-31 12:34:30 -0300

    feat: Implementa validacion de credenciales

--- app/src/main/java/com/levelup/levelupgamer/db/repository/UsuarioRepository.kt
+++ app/src/main/java/com/levelup/levelupgamer/db/repository/UsuarioRepository.kt
@@ -2,16 +2,34 @@ package com.levelup.levelupgamer.db.repository
 
 import com.levelup.levelupgamer.db.dao.UsuarioDAO
 import com.levelup.levelupgamer.db.entidades.Usuario
+import com.levelup.levelupgamer.utils.CifradoContrasena
 import javax.inject.Inject
 
 class UsuarioRepository @Inject constructor(
     private val usuarioDAO: UsuarioDAO
 ) {
     suspend fun crearCuenta(usuario: Usuario): Long {
-        return usuarioDAO.agregarUsuario(usuario)
+        val contrasenaCifrada = CifradoContrasena.cifrarContrasena(usuario.contrasena)
+        val nuevoUsuario = usuario.copy(contrasena = contrasenaCifrada)
+
+        return usuarioDAO.agregarUsuario(nuevoUsuario)
     }
 
     suspend fun buscarUsuarioPorCorreo(correo: String): Boolean {
         return usuarioDAO.obtenerUsuarioPorCorreo(correo) != null
     }
+
+    suspend fun validarCredenciales(correo: String, contrasenaIngresada: String): Boolean {
+        val usuario = usuarioDAO.obtenerUsuarioPorCorreo(correo)
+
+        if (usuario == null) {
+            return false
+        }
+
+        val contrasenaCifrada = usuario.contrasena
+        val esValido = CifradoContrasena.verificarContrasena(contrasenaIngresada, contrasenaCifrada)
+
+        return esValido
+    }
+
 }
--- app/src/main/java/com/levelup/levelupgamer/utils/CifradoContrasena.kt
+++ app/src/main/java/com/levelup/levelupgamer/utils/CifradoContrasena.kt
@@ -1,2 +1,17 @@
 package com.levelup.levelupgamer.utils
 
+import org.mindrot.jbcrypt.BCrypt
+
+object CifradoContrasena {
+    fun cifrarContrasena(contrasena: String): String {
+        return BCrypt.hashpw(contrasena, BCrypt.gensalt(12))
+    }
+
+    fun verificarContrasena(contrasena: String, contrasenaCifrada: String): Boolean {
+        return try {
+            BCrypt.checkpw(contrasena, contrasenaCifrada)
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
